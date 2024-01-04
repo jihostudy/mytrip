d24d237b69d15d80e9c22d909d20fd7e930536b2
Ji__ho <cale.jiho@gmail.com>
2024-01-04 16:54:53 +0900

    검색지역 이동완료

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 // Components
 import KakaoMap from "../components/KakaoMap";
 
@@ -6,18 +6,21 @@ const PlanningPage = () => {
   const [userInput, setUserInput] = useState(null);
   const input = useRef(null);
 
-  const clickHandler = (e) => {
-    e.preventDeafult();
+  const submitHandler = (e) => {
+    e.preventDefault();
     setUserInput(input.current.value);
   };
+
+  useEffect(() => {
+    console.log("changed");
+  }, [userInput]);
   return (
     <main className="relative flex h-dvh w-full items-center justify-between">
       {/* 유저 입력 컴포넌트 자리 (임시 코드)*/}
-
-      <input ref={input} id="user-input" className="border-2 border-black" />
-      <button type="submit" onClick={(e) => clickHandler(e)}>
-        검색버튼
-      </button>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input ref={input} id="user-input" className="border-2 border-black" />
+        <button type="submit">검색버튼</button>
+      </form>
       <KakaoMap userInput={userInput} />
     </main>
   );
