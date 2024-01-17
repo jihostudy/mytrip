9af5362244095ee9eab1144ecba91f93ef2bd25e
Ji__ho <cale.jiho@gmail.com>
2024-01-17 21:34:40 +0900

    여행지 선택 초안

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
+// Dummy areas
+const dummy_images = ["img1", "img2", "img3", "img4"];
+
 const HomePage = () => {
+  const imgContanier = dummy_images.map((img) => (
+    <li key={img} className="m-2">
+      {img}
+    </li>
+  ));
   return (
-    <main className="flex h-dvh w-full items-center justify-center bg-stone-300">
-      여기는 홈페이지 영역입니다.
+    <main className="flex h-dvh w-full flex-col items-center justify-center">
+      <form>
+        <input
+          type="text"
+          placeholder="여행지를 입력해주세요"
+          id="region-list"
+        />
+        <datalist id="region-list">
+          <option value="제주도" />
+          <option value="부산" />
+          <option value="경주" />
+          <option value="울산" />
+          <option value="서울" />
+        </datalist>
+      </form>
+      <div>
+        <ul className="flex justify-evenly">{imgContanier}</ul>
+      </div>
     </main>
   );
 };
