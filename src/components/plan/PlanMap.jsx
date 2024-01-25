9993fb9429ce6ee0007d55e025a37003282aeed3
Jinjeon Shin <sjg729729@gmail.com>
2024-01-25 16:05:07 +0900

    PlanningPage 초안

---
+++ src/components/plan/PlanMap.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlanMap = () => {
+  return (
+    <div className="h-full w-[32%] border border-solid border-black">
+      PlanMap
+    </div>
+  );
+};
+
+export default PlanMap;
--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+// Components
+import PlanHeader from "../components/plan/PlanHeader";
+import PlanPlace from "../components/plan/PlanPlace";
+import PlanTimeTable from "../components/plan/PlanTimeTable";
+import PlanMap from "../components/plan/PlanMap";
+
 const PlanningPage = () => {
-  return <main>여기는 여행만들기 페이지 입니다.</main>;
+  return (
+    <div className="absolute top-[15%] flex h-full w-full flex-col">
+      <div className="h-[10%]">
+        <PlanHeader />
+      </div>
+
+      <div className="flex h-[65%] w-full items-center justify-center">
+        <PlanPlace />
+        <PlanTimeTable />
+        <PlanMap />
+      </div>
+    </div>
+  );
 };
 
 export default PlanningPage;
