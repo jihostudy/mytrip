0f36bfa1167002110c1792f75e7f342a4c1231ea
Jinjeon Shin <sjg729729@gmail.com>
2024-02-06 14:00:23 +0900

    닉네임 변경, 회원 탈퇴 완료

--- src/pages/UserInfoPage.jsx
+++ src/pages/UserInfoPage.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 
+// custom hook
+import { useConfirmModal } from "../hook/useConfirmModal";
+
 // API
 import { API } from "../api/API";
 
@@ -9,16 +12,34 @@ import { useRecoilState } from "recoil";
 
 // icon
 import NameChangeIcon from "../assets/icons/NameChange.svg?react";
+import { useNavigate } from "react-router-dom";
+import { is } from "date-fns/locale";
+
+// message
+const VALID_USERNAME_MESSAGE = "사용가능한 닉네임입니다";
+const INVALID_USERNAME_MESSAGE = "닉네임 중복검사를 다시해주세요";
+const DELETE_MESSAGE =
+  "현재 사용중인 계정 정보는 복구 불가능합니다.\n커뮤니티에 등록한 게시물은 삭제되지 않고 유지됩니다. 삭제를 원하는 게시물이 있을 경우, 반드시 삭제 후 탈퇴 하시기 바라며, 필요한 데이터는 미리 백업을 해주시기 바랍니다.";
 
 const UserInfoPage = () => {
+  const [userEmail, setUserEmail] = useState();
   const [userInfo, setUserInfo] = useRecoilState(user);
   const [isNameChange, setIsNameChange] = useState(false);
+  const [isUserNameValid, setIsUserNameValid] = useState(0); // 0 : 초기, 1 : 중복, 2 : 사용가능
+  const [isSNS, setIsSNS] = useState(false);
+
+  // modal hook
+  const { openModal } = useConfirmModal();
+
+  // navigate
+  const navigate = useNavigate();
 
   // ref
   const usernameRef = useRef();
 
-  // username handler
+  // ----------------- username handler -----------------
   async function nameCheckHandler() {
+    // 중복 체크
     console.log("중복 체크");
     try {
       const res = await API.post("/auth/verify/username", {
@@ -27,6 +48,7 @@ const UserInfoPage = () => {
       const status = res.status;
       if (status === 200) {
         // 사용 가능한 닉네임
+        setIsUserNameValid(2);
       }
       console.log(status);
     } catch (e) {
@@ -35,83 +57,176 @@ const UserInfoPage = () => {
       console.log(status);
       if (status === 409) {
         // 중복된 닉네임
+        setIsUserNameValid(1);
       } else {
         // 서버 오류
+        console.log("server error");
       }
     }
   }
   async function nameChangeHandler() {
+    // 닉네임 변경
     console.log("닉네임 변경");
     try {
       const res = await API.post("/auth/updateUsername", {
         username: usernameRef.current.value,
       });
       const status = res.status;
-      if (status === 200) {
+      if (isUserNameValid !== 2) {
+        setIsUserNameValid(1);
+        console.log("invlaid username");
+        return;
+      }
+
+      if (status === 200 && isUserNameValid === 2) {
         localStorage.removeItem("accessToken");
         localStorage.setItem("accessToken", res.headers.get("Authorization"));
         setUserInfo(() => ({
           isLogin: true,
           username: usernameRef.current.value,
         }));
+        console.log("change username");
+        setIsUserNameValid(0);
+        setIsNameChange(false);
         // 닉네임 변경 완료
       }
     } catch (e) {
       const status = e.response.status;
-      if (status === 500) {
-        // 서버 오류
-      } else {
-        console.log("서버 오류");
-      }
+      // 500 서버 오류
+      console.log(status, "server error");
     }
   }
+  // username message;
+  let usernameMessage = "";
+  if (isUserNameValid === 0) {
+    usernameMessage = <p>&nbsp;</p>;
+  }
+  if (isUserNameValid === 1) {
+    usernameMessage = (
+      <p className="text-xs text-[#EB4315]">{INVALID_USERNAME_MESSAGE}</p>
+    );
+  }
+  if (isUserNameValid === 2) {
+    usernameMessage = (
+      <p className="text-xs text-[#38C3FF]">{VALID_USERNAME_MESSAGE}</p>
+    );
+  }
 
-  // password handler
+  // ----------------- password handler -----------------
+  // 수정하기 클릭
+  //
 
-  // delete account handler
+  // ----------------- delete account handler -----------------
+  function deleteClick() {
+    // modal
+    openModal({
+      content: DELETE_MESSAGE,
+      callback: () => {
+        deleteHandler();
+      },
+    });
+  }
 
-  // 유저 정보 저장
+  async function deleteHandler() {
+    // API
+    try {
+      const res = await API.delete("/auth/signout");
+      const status = res.status;
+      if (status === 200) {
+        console.log("회원 탈퇴");
+      }
+    } catch (error) {
+      const status = error.response.status;
+      if (status === 500) {
+        console.log(status, "server error");
+      } else if (status === 404) {
+        console.log((status, "User not found"));
+      }
+    }
+
+    // recoil 수정
+    setUserInfo(() => ({
+      isLogin: false,
+      username: null,
+    }));
+
+    // 토큰 삭제
+    localStorage.removeItem("accessToken");
+    // 리다이렉트
+    navigate("/start");
+  }
+
+  // 이메일 저장
   useEffect(() => {
-    // async function getInfo() {
-    //   try {
-    //     const res = await API.get("/my-page/profile");
-    //     const data = { email: res.data.email, username: res.data.username };
-    //     setUserInfo(data);
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
-    // }
-    // getInfo();
+    async function getEmail() {
+      try {
+        const res = await API.get("/my-page/profile");
+        setUserEmail(res.data.email);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getEmail();
   }, []);
-  // console.log(userInfo);
-
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex h-[60%] flex-col items-center justify-center gap-1">
       {/* 닉네임 변경 */}
-      <div className="flex justify-center">
-        <p>닉네임</p>
+      <div className="flex h-[7%] w-[50%] justify-between gap-2">
+        <p className="flex w-[20%] items-center">닉네임</p>
         {!isNameChange ? (
-          <>
-            <p>{userInfo.username}</p>
-            <NameChangeIcon
-              onClick={() => setIsNameChange((prev) => !prev)}
-              className="hover:cursor-pointer"
-            />
-          </>
+          <p className="flex grow items-center">{userInfo.username}</p>
         ) : (
           <>
-            <input placeholder="입력해주세요" ref={usernameRef} />
-            <button onClick={nameCheckHandler}>중복확인</button>
-            <button onClick={nameChangeHandler}>확인</button>
+            <input
+              className="grow"
+              placeholder="입력해주세요"
+              ref={usernameRef}
+            />
           </>
         )}
+        {!isNameChange ? (
+          <div className="flex w-[25%]">
+            <button
+              onClick={() => setIsNameChange((prev) => !prev)}
+              className="rounded-md border-1 border-solid border-black px-2 text-sm hover:bg-slate-100"
+            >
+              수정
+            </button>
+          </div>
+        ) : (
+          <div className="relative flex w-[25%] gap-5">
+            <button
+              onClick={nameCheckHandler}
+              className="rounded-md border-1 border-solid border-black px-2 text-sm hover:bg-slate-100"
+            >
+              중복확인
+            </button>
+            <button
+              onClick={nameChangeHandler}
+              className="absolute left-2/3 aspect-square w-[60%] rounded-[50%] border-[1px] border-solid border-black bg-[#FFCB16] shadow-[0_0_40px_0px_#FFCB16]"
+            >
+              변경
+            </button>
+          </div>
+        )}
       </div>
+      {/* 닉네임 확인 문구 */}
+      <div className="mb-1 w-[50%]">{usernameMessage}</div>
       {/* 이메일 표시 */}
-      <div>이메일</div>
-      {/* 비밀번호 변경 */}
-      <div>비밀번호</div>
-      {/* 회원 탈퇴 */}
-      <div>회원탈퇴</div>
+      <div className="flex h-[7%] w-[50%] justify-start gap-2">
+        <p className="flex w-[20%] items-center">이메일</p>
+        <p className="flex items-center text-[#00000066]">{userEmail}</p>
+      </div>
+      {/* 비밀번호 변경, 회원 탈퇴  */}
+      <div className="mt-10 flex w-[50%] gap-4">
+        <div className="text-sm text-[#00000066] underline">비밀번호</div>
+        <div
+          onClick={deleteClick}
+          className="text-sm text-[#00000066] underline hover:cursor-pointer"
+        >
+          회원탈퇴
+        </div>
+      </div>
     </div>
   );
 };
